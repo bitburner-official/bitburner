f8d75d5069dd3b17998cc1fa35767ad89d796d10
Aleksander Heintz <alxandr@alxandr.me>
2018-09-12 22:39:55 +0200

    Hide the async function

--- src/utils/delayed-single.ts
+++ src/utils/delayed-single.ts
@@ -1,11 +1,14 @@
 import { Host, BitBurner as NS } from 'bitburner';
 
-const delayedSingle = (fn: (ns: NS, host: Host) => Promise<any>) => async (
-  ns: NS,
-) => {
-  const [host, time] = ns.args;
-  await ns.sleep(parseInt(time, 10) - Date.now());
-  await fn(ns, host);
+const delayedSingle = (fn: (ns: NS, host: Host) => Promise<any>) => {
+  // hide the async function
+  const delayed = async (ns: NS) => {
+    const [host, time] = ns.args;
+    await ns.sleep(parseInt(time, 10) - Date.now());
+    await fn(ns, host);
+  };
+
+  return (ns: NS) => delayed(ns);
 };
 
 export default delayedSingle;
